6e7f8dc927b35f02fbe32fac3c243354cb103f69
betsky2112 <roberthussebastians@gmail.com>
2024-05-15 22:10:04 +0700

    Fetch Pokemon API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import PokeItem from './components/PokeItem'
 
 const App = () => {
-	return <div>App</div>
+	const [pokemonList, setPokemonList] = useState([])
+	const [selectedPokemonName, setSelectedPokemonName] = useState('')
+
+	useEffect(() => {})
+	fetch('https://pokeapi.co/api/v2/pokemon?limit=10').then((res) => {
+		res.json()
+			.then((data) => setPokemonList(data.results))
+			.catch((err) => console.log(err))
+	}, [])
+	return (
+		<div style={styles.container}>
+			<h1>Pokemon API</h1>
+			<PokeItem
+				pokemonList={pokemonList}
+				setSelectedPokemonName={setSelectedPokemonName}
+			/>
+			<p>{selectedPokemonName}</p>
+		</div>
+	)
 }
 
+const styles = {
+	container: {
+		width: '50%',
+		margin: '0 auto',
+		padding: '80px',
+		textAlign: 'center',
+	},
+}
 export default App
